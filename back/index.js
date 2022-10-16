c1449b7bde9e69487c3fce546117123dad8fa220
pseudobear <2002michaelli@gmail.com>
2022-10-15 23:44:14 -0400

    uri encode for db connection

--- back/index.js
+++ back/index.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 const {MongoClient} = require("mongodb");
 const port = 3001;
 
-const DBuser = readline.question("please enter DB username");
-const DBpass = readline.question("please enter DB password");
+const DBuser = encodeURIComponent(readline.question("please enter DB username: "));
+const DBpass = encodeURIComponent(readline.question("please enter DB password: "));
 
 const mongoClient = new MongoClient(`mongodb+srv://${DBuser}:${DBpass}@cluster0.uybyndw.mongodb.net/?retryWrites=true&w=majority`);
 
